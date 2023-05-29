6ffe420a0c6d93f435de9e800a7873d26e39f058
libbyschuknight <libbysk@gmail.com>
2023-05-30 08:45:17 +1200

    Mod 10 - 59 completed

--- exercises/59 - Slider/src/index.js
+++ exercises/59 - Slider/src/index.js
@@ -20,12 +20,51 @@ function Slider(slider) {
   }
 
   function applyClasses() {
+    current.classList.add('current');
+    prev.classList.add('prev');
+    next.classList.add('next');
   }
 
-  // 22mins02secs
+  function move(direction) {
+    // first strip all the classes off the current slides
+    const classesToRemove = ['prev', 'current', 'next'];
+    prev.classList.remove(...classesToRemove);
+    current.classList.remove(...classesToRemove);
+    next.classList.remove(...classesToRemove);
+
+    if (direction === 'back') {
+      // make an new array of the new values, and destructure them over and into the prev, current and next variables
+      [prev, current, next] = [
+        // first thing in this array will be prev
+        // second will be current
+        // third will be next
+        // get the prev slide, if there is none, get the last slide from the entire slider for wrapping
+        prev.previousElementSibling || slides.lastElementChild,
+        prev,
+        current,
+      ];
+      // // 2 => 1, 3 => 2, 4 => 3
+      // [2, 3, 4] = [1, 2, 3];
+    } else {
+      [prev, current, next] = [
+        current,
+        next,
+        // get the next slide, or if it's at the end, loop around and grab the first slide
+        next.nextElementSibling || slides.firstElementChild,
+      ];
+    }
+
+    applyClasses();
+  }
+
 
   // when this slider is created, run the start slider function
   startSlider();
+  applyClasses();
+
+  // Event listeners
+  prevButton.addEventListener('click', () => move('back'));
+  nextButton.addEventListener('click', move);
 }
 
 
