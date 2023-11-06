77a5601a1ecb152b97cc240673907f290b7bdbc3
libbyschuknight <libbysk@gmail.com>
2023-11-07 08:49:17 +1300

    Mod 13 - 74 started

--- exercises/75 - CORS and Recipes/scripts.js
+++ exercises/75 - CORS and Recipes/scripts.js
@@ -1,2 +1,40 @@
 // The Recipe Puppy API used in the course is broken
 // Please use this replacement API URL "https://recipes.beginnerjavascript.com/api"
+
+// gitHub API
+// const data = `{
+//   "login": "libbyschuknight",
+//   "id": 7097235,
+//   "node_id": "MDQ6VXNlcjcwOTcyMzU=",
+//   "avatar_url": "https://avatars.githubusercontent.com/u/7097235?v=4",
+//   "gravatar_id": "",
+//   "url": "https://api.github.com/users/libbyschuknight",
+//   "html_url": "https://github.com/libbyschuknight",
+//   "followers_url": "https://api.github.com/users/libbyschuknight/followers",
+//   "following_url": "https://api.github.com/users/libbyschuknight/following{/other_user}",
+//   "gists_url": "https://api.github.com/users/libbyschuknight/gists{/gist_id}",
+//   "starred_url": "https://api.github.com/users/libbyschuknight/starred{/owner}{/repo}",
+//   "subscriptions_url": "https://api.github.com/users/libbyschuknight/subscriptions",
+//   "organizations_url": "https://api.github.com/users/libbyschuknight/orgs",
+//   "repos_url": "https://api.github.com/users/libbyschuknight/repos",
+//   "events_url": "https://api.github.com/users/libbyschuknight/events{/privacy}",
+//   "received_events_url": "https://api.github.com/users/libbyschuknight/received_events",
+//   "type": "User",
+//   "site_admin": false,
+//   "name": "Libby Schumacher-Knight",
+//   "company": null,
+//   "blog": "https://medium.com/@SchuKnight",
+//   "location": "New Zealand",
+//   "email": null,
+//   "hireable": true,
+//   "bio": "Developer. Ruby, Rails, HTML, CSS, JS, Vue",
+//   "twitter_username": null,
+//   "public_repos": 96,
+//   "public_gists": 45,
+//   "followers": 16,
+//   "following": 23,
+//   "created_at": "2014-03-29T00:14:29Z",
+//   "updated_at": "2023-10-15T04:15:46Z"
+// }`
+
+// const dataObj = JSON.parse(data);
