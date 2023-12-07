178be5493a64572851c8212c71d15a6edbd1657b
libbyschuknight <libbysk@gmail.com>
2023-12-08 08:24:35 +1300

    Mod 14 - 83 completed

--- exercises/83 - Security/nasty.js
+++ exercises/83 - Security/nasty.js
@@ -1,17 +1,24 @@
+// The rule is that anytime you are taking data from the user and embedding it in HTML you must first sanitize it.
+// Anywhere that you are using.innerHTML or using insertAdjacentHTML, or really anywhere that you are taking user data and generating HTML from that, you must first sanitize the data.
+
 import { sanitize } from 'dompurify';
 
 const input = document.querySelector('[name="input"]');
 const output = document.querySelector('.output');
 const buttons = document.querySelectorAll('nav button');
 input.addEventListener('input', () => {
-  output.innerHTML = input.value.replace(/\n/g, '<br>');
+  const clean = sanitize(input.value, {
+    FORBID_ATTR: ['width', 'height', 'style'],
+    FORBID_TAGS: ['style'],
+  });
+  output.innerHTML = clean.replace(/\n/g, '<br>');
 });
 
 // trigger an input even on page load
 input.dispatchEvent(new Event('input'));
 
-buttons.forEach(button =>
-  button.addEventListener('click', e => {
+buttons.forEach((button) =>
+  button.addEventListener('click', (e) => {
     alert(e.currentTarget.textContent);
   })
 );
